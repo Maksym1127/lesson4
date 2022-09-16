ef299a11bf5eeaff774cc01af110fb8a1becd20f
Maksym1127 <maksimchuk004@gmail.com>
2022-09-16 18:17:00 +0300

    фігура

---
+++ src/papka/Shape.java
@@ -0,0 +1,13 @@
+package papka;
+
+public class Shape {
+    public static void main(String[] args) {      // *******
+        int row = 7;                              //  *****
+        for (int i = 0; i <= row; i++) {          //   ***
+            System.out.print("* ");               //    *
+
+        }
+
+    }
+
+}
